c9c1b75f17a9141af4badd0197a44308c47eb7eb
sridharmangineni1 <manginenisridhar94@gmail.com>
2024-08-31 23:27:51 -0500

    findPairsWithSum.cpp

--- findPairsWithSum.cpp
+++ findPairsWithSum.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-
 using namespace std;
 
 vector<int> findPairsWithSum(vector<int>& nums, int targetSum) {
@@ -9,12 +8,11 @@ vector<int> findPairsWithSum(vector<int>& nums, int targetSum) {
 
     for (int currentIndex = 0; currentIndex < nums.size(); currentIndex++) {
         int requiredValue = targetSum - nums[currentIndex];
-       
+
         if (valueToIndexMap.find(requiredValue) != valueToIndexMap.end()) {
-            return { valueToIndexMap[requiredValue], currentIndex }; 
+            return {valueToIndexMap[requiredValue], currentIndex};
         }
 
-    
         valueToIndexMap[nums[currentIndex]] = currentIndex;
     }
 
@@ -22,17 +20,17 @@ vector<int> findPairsWithSum(vector<int>& nums, int targetSum) {
 }
 
 int main() {
-    vector<int> sampleNumbers1 = { 2, 7, 11, 15 };
+    vector<int> sampleNumbers1 = {2, 7, 11, 15};
     int target1 = 9;
     vector<int> result1 = findPairsWithSum(sampleNumbers1, target1);
     cout << "[" << result1[0] << ", " << result1[1] << "]" << endl;
 
-    vector<int> sampleNumbers2 = { 3, 2, 4 };
+    vector<int> sampleNumbers2 = {3, 2, 4};
     int target2 = 6;
     vector<int> result2 = findPairsWithSum(sampleNumbers2, target2);
     cout << "[" << result2[0] << ", " << result2[1] << "]" << endl;
 
-    vector<int> sampleNumbers3 = { 3, 3 };
+    vector<int> sampleNumbers3 = {3, 3};
     int target3 = 6;
     vector<int> result3 = findPairsWithSum(sampleNumbers3, target3);
     cout << "[" << result3[0] << ", " << result3[1] << "]" << endl;
